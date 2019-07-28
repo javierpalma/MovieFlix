f16d81f5f1159f4413ed3488c84473aa79cb9dd0
Edmund Home <edmundhome@hotmail.com>
2019-07-28 11:58:48 +0200

    Create Peliculas.java

---
+++ MovieFlix/src/beans/Peliculas.java
@@ -0,0 +1,14 @@
+package beans;
+
+public class Peliculas {
+	
+	private String putaBidaTete;
+
+	public String getPutaBidaTete() {
+		return putaBidaTete;
+	}
+
+	public void setPutaBidaTete(String putaBidaTete) {
+		this.putaBidaTete = putaBidaTete;
+	}
+}
