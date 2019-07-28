fce5c43afea36fa94864d0328c1934f8ae95c667
Edmund Home <edmundhome@hotmail.com>
2019-07-28 11:51:45 +0200

    Update Clientes.java

--- MovieFlix/src/beans/Clientes.java
+++ MovieFlix/src/beans/Clientes.java
@@ -1,5 +1,14 @@
 package beans;
 
 public class Clientes {
+	private String mecadoento;
+
+	public String getMecadoento() {
+		return mecadoento;
+	}
+
+	public void setMecadoento(String mecadoento) {
+		this.mecadoento = mecadoento;
+	}
 
 }
