520a59be15954791609f913c2d71df3595953ca7
Edmund Home <edmundhome@hotmail.com>
2019-07-29 12:10:32 +0200

    Update Categoria.java

--- MovieFlix/src/beans/Categoria.java
+++ MovieFlix/src/beans/Categoria.java
@@ -1,6 +1,9 @@
 package beans;
 
+//Se crea la clase categoría con atributos id y nombre.
+
 public class Categoria {
+	
 	
 	private int id;
 	private String nombre;
@@ -8,28 +11,49 @@ public class Categoria {
 	public Categoria() {
 		super();
 	}
-
+ /**
+  * 
+  * @param id
+  * @param nombre
+  */
 	public Categoria(int id, String nombre) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 	}
-
+/**
+ * 
+ * @return id
+ */
 	public int getId() {
 		return id;
 	}
-
+/**
+ * @param id
+ */
 	public void setId(int id) {
 		this.id = id;
 	}
-
+/**
+ * 
+ * @return nombre
+ */
 	public String getNombre() {
 		return nombre;
 	}
+	
+	/**
+	 * 
+	 * @param nombre
+	 */
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	/**
+	 * 
+	 */
 
 	@Override
 	public String toString() {
