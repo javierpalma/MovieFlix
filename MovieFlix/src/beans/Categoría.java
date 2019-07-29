efa2981a94f362ffd950813183a0b94a9eba5b22
Edmund Home <edmundhome@hotmail.com>
2019-07-29 11:53:17 +0200

    Create Categoría.java

---
+++ MovieFlix/src/beans/Categoría.java
@@ -0,0 +1,39 @@
+package beans;
+
+public class Categoría {
+	
+	private int id;
+	private String nombre;
+	
+	public Categoría() {
+		super();
+	}
+
+	public Categoría(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoría [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+}
