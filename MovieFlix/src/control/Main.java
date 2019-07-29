842fcc14e14054b8ed93663f0e43bde0e178249f
Edmund Home <edmundhome@hotmail.com>
2019-07-29 13:47:10 +0200

    volcadoDatos

--- MovieFlix/src/control/Main.java
+++ MovieFlix/src/control/Main.java
@@ -1,11 +1,15 @@
 package control;
 
+import servicios.ConectarBD;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ServiciosMovieFlix.arrancarAplicacion();
+		//ServiciosMovieFlix.arrancarAplicacion();
+		ConectarBD con = new ConectarBD();
+		con.volcarDatos();
 	}
 
 }
--- MovieFlix/src/servicios/ConectarBD.java
+++ MovieFlix/src/servicios/ConectarBD.java
@@ -20,6 +20,7 @@ public class ConectarBD {
 	
 	private String bd;
 	String url = "jdbc:mysql://10.90.36.103:3306/";
+	String dataZone= "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	String user = "movieflix";
 	String password = "movieflix";
 	
@@ -30,7 +31,7 @@ public class ConectarBD {
 	//el metodo conectarBD devuelve la conexión a la bd
 	public Connection conectarBD(String bd) {
 		Connection con = null;
-		this.bd = url+bd;
+		this.bd = url+bd+this.dataZone;
 		try {
 			con = DriverManager.getConnection(this.bd, user, password);
 			return con;
@@ -45,16 +46,23 @@ public class ConectarBD {
 		File f_txt = new File("peliculas_numCat.txt");
 		Connection con;
 		PreparedStatement pst = null;
+		ResultSet rs= null;
 		String linea;
 		
 		try {
 			Reader r = new FileReader(f_txt);
 			BufferedReader br = new BufferedReader (r);
 			linea = br.readLine();
+			con=this.conectarBD("movieflix");
+			System.out.println("Conectado a BD");
+			System.out.println(linea);
 			do{
 				String[]datos = linea.split(",");
-				con=this.conectarBD("movieflix");
-				pst = con.prepareStatement("INSERT INTO pelicula(nombre,anyoEstreno,id_categoria VALUES ("+datos[0]+","+datos[1]+","+datos[2]+")");
+				//System.out.println(datos[0]);
+				pst = con.prepareStatement("INSERT INTO pelicula (nombre_pelicula,anyo_estreno,id_categoria) VALUES ('"+datos[0]+"',"+datos[1]+","+datos[2]+");");
+				//System.out.println(pst);
+				pst.executeUpdate();
+				//System.out.println(pst.executeUpdate());
 				linea = br.readLine();
 				
 			}while(linea !="FEOF");
