0cd8d6027936b0cf5a9f605cc3904c5c0099cc1e
Edmund Home <edmundhome@hotmail.com>
2019-07-29 12:38:55 +0200

    Create ServiciosMovieFlix.java

---
+++ MovieFlix/src/servicios/ServiciosMovieFlix.java
@@ -0,0 +1,27 @@
+package servicios;
+
+public class ServiciosMovieFlix implements I_ServiciosMovieFlix {
+
+	
+	
+	@Override
+	public void altaPelicula() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean bajaPelicula() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificarPelicula() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+}
