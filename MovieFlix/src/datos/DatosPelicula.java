b4be5327df690fc071be5d72695ef733090f4584
Josemi <josemiguelteruelrebollo@gmail.com>
2019-07-29 12:53:29 +0200

    Update DatosPelicula.java

--- MovieFlix/src/datos/DatosPelicula.java
+++ MovieFlix/src/datos/DatosPelicula.java
@@ -1,5 +1,15 @@
 package datos;
 
+import java.awt.List;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import beans.Pelicula;
+import servicios.ConectarBD;
+
+import com.mysql.cj.xdevapi.Statement;
+
 import beans.Pelicula;
 
 /**
@@ -40,6 +50,34 @@ public class DatosPelicula {
 	}
 	
 	
-	
+	public static void obtenerPelicula(Pelicula pelicula) {
+		Connection co =null;
+		Statement stm= null;
+		ResultSet rs=null;
+		
+		//String sql="SELECT * FROM CLIENTE ORDER BY ID";
+		
+		ArrayList<Pelicula> listaCliente= new ArrayList<Pelicula>();
+		
+		try {			
+			co= ConectarBD.conectarBD("movieflix");
+			stm=co.createStatement();
+			rs=stm.executeQuery(sql);
+			while (rs.next()) {
+				Cliente c=new Cliente();
+				c.setId(rs.getInt(1));
+				c.setCedula(rs.getString(2));
+				c.setNombre(rs.getString(3));
+				c.setApellido(rs.getString(4));
+				listaCliente.add(c);
+			}
+			stm.close();
+			rs.close();
+			co.close();
+		} catch (SQLException e) {
+			System.out.println("Error: Clase ClienteDaoImple, método obtener");
+			e.printStackTrace();
+		}
+	}
 
 }
