d31c05e21c7edb3778791e3331085659ab71fedd
Edmund Home <edmundhome@hotmail.com>
2019-07-29 12:51:23 +0200

    Update ConectarBD.java

--- MovieFlix/src/servicios/ConectarBD.java
+++ MovieFlix/src/servicios/ConectarBD.java
@@ -4,6 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import java.io.File;
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 /**
 *
 * @author Javier Palma
@@ -12,13 +17,16 @@ import java.sql.SQLException;
 public class ConectarBD {
 	
 	private String bd;
-	String url = "jdbc:mysql://localhost:3306/";
+	String url = "jdbc:mysql://10.90.36.103:3306/";
 	String user = "movieflix";
 	String password = "movieflix";
 	
-	
+	public ConectarBD() {
+		super();
+	}
+
 	//el metodo conectarBD devuelve la conexión a la bd
-	public Connection  conectarBD(String bd) {
+	public Connection conectarBD(String bd) {
 		Connection con = null;
 		this.bd = url+bd;
 		try {
@@ -30,4 +38,27 @@ public class ConectarBD {
 		}
 		return null;
 	} 
+	
+	public void volcarDatos() {
+		File f_txt = new File("peliculas_numCat.txt");
+		Connection con;
+		String linea;
+		
+		try {
+			Reader r = new FileReader(f_txt);
+			BufferedReader br = new BufferedReader (r);
+			do{
+				linea = br.readLine();
+				String[]datos = linea.split(",");
+				con=this.conectarBD("movieflix");
+				
+				
+			}while(linea !="FEOF");
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		
+		
+		
+	}
 }
