457000579d0464d74a430275a15dbebb70a6c033
Edmund Home <edmundhome@hotmail.com>
2019-07-30 10:21:18 +0200

    g

--- MovieFlix/src/utilidades/PedirUsuario.java
+++ MovieFlix/src/utilidades/PedirUsuario.java
@@ -1,5 +1,12 @@
 package utilidades;
 
 public class PedirUsuario {
+	
+	public static void pedirUsuario() {
+		System.out.println("Nombre usuario: ");
+		String nombre=new Scanner(System.in).nextLine();
+		System.out.println("");
+		System.out.println("");
+	}
 
 }
