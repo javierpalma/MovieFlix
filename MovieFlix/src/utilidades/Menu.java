f9ce3b27c00c1d6fda9194b80a6d7c935103f171
Josemi <josemiguelteruelrebollo@gmail.com>
2019-07-30 10:49:18 +0200

    Update Menu.java

--- MovieFlix/src/utilidades/Menu.java
+++ MovieFlix/src/utilidades/Menu.java
@@ -12,7 +12,6 @@ public class Menu {
 		System.out.println("7. Informes");
 		System.out.println("0. Salir");
 	}
-<<<<<<< HEAD
 	
 	public static void listarInformes() {
 		
@@ -23,7 +22,7 @@ public class Menu {
 		System.out.println("5 - Marcar Pelicula cliente como vista");		
 		System.out.println("0 - Salirr");
 	}
-=======
->>>>>>> c20230cdcd70a18faa54085ecabd0e57b547d87b
+
+
 
 }
