c75f0591c30aa758fbc2b25478bcc625d831c3a2
Edmund Home <edmundhome@hotmail.com>
2019-07-30 20:26:47 +0200

    cambios

--- MovieFlix/src/control/Main.java
+++ MovieFlix/src/control/Main.java
@@ -1,14 +1,5 @@
 package control;
 
-import java.time.LocalDate;
-
-import beans.Categoria;
-import beans.Cliente;
-import beans.Pelicula;
-import datos.DatosCliente;
-import datos.DatosPelicula;
-import datos.GenerarInforme;
-import servicios.ConectarBD;
 import servicios.ServiciosMovieFlix;
 
 public class Main {
