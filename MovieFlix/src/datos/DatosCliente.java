5f81a226207682d2cd4e430ce5f9568ef1540b4b
Josemi <josemiguelteruelrebollo@gmail.com>
2019-07-30 12:22:49 +0200

    Update DatosCliente.java

--- MovieFlix/src/datos/DatosCliente.java
+++ MovieFlix/src/datos/DatosCliente.java
@@ -3,7 +3,12 @@ package datos;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Logger;
+import java.time.LocalDate;
+
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import beans.Cliente;
 import beans.Pelicula;
@@ -59,50 +64,50 @@ public class DatosCliente {
 		return "DatosCliente [cliente=" + cliente + "]";
 	}
 	
-	
-	public Cliente obtenerCliente(String nombre) {
-		int flag = -1;
-		Connection co =null;
-		ConectarBD conect = new ConectarBD();
-		java.sql.Statement stm= null;
-		ResultSet rs=null;
-		
-		String sql="";
-		Cliente c = new Cliente();
-		
-		
-		try {			
-			co= conect.conectarBD("movieflix") ;
-			stm=co.createStatement();
-			rs=stm.executeQuery(sql);
-			
-			while (rs.next()) {
-				
-				c.setIdCliente(rs.getInt);
-				c.setNombreCliente(rs.getString());
-				
-		
-				if(c.getNombre().trim().equalsIgnoreCase(nombre)) {
-					System.out.println("");
-					return c.getNmbre();
-				}
-				else {
-					flag= -1;
-				}
-			}
-			stm.close();
-			rs.close();
-			co.close();
-			
-		} catch (SQLException e) {
-			System.out.println("Error: Clase DatosCliente, método obtenerCliente");
-			e.printStackTrace();
-			Logger lgr = Logger.getLogger(nombre);
-            lgr.log(Level.INFO, "FALLO EN PARÁMETRO NOMBRE, MÉTODO obtenerCliente");
-		}
-		
-		return c.getNombre();
-	}
+
+			public Cliente obtenerCliente(String nombre) {
+			        int flag = -1;
+			        Connection co =null;
+			        ConectarBD conect = new ConectarBD();
+			        java.sql.Statement stm= null;
+			        ResultSet rs=null;
+			        Logger logger = LogManager.getLogger(); 
+			        
+			        String sql="SELECT * FROM CLIENTE WHERE NOMBRE CLIENTE=NOMBRE";
+			        Cliente c = new Cliente();
+			        
+			        
+			        try {            
+			            co= conect.conectarBD("movieflix") ;
+			            stm=co.createStatement();
+			            rs=stm.executeQuery(sql);
+			            
+			            while (rs.next()) {
+			                
+			                c.setIdCliente(rs.getInt(1));
+			                c.setNombreCliente(rs.getString(2));
+			                LocalDate fecha = rs.getDate(3).toLocalDate();
+			                c.setFechaNacimiento(fecha);
+			                c.setCiudad(rs.getString(4));
+			                
+			        
+			                if(c.getNombreCliente().trim().equalsIgnoreCase(nombre)) {
+			                    System.out.println("El cliente existe");
+			                    return c;
+			                }
+			                
+			            }
+			            stm.close();
+			            rs.close();
+			            co.close();
+			            
+			        } catch (SQLException e) {
+			        	
+			            logger.info(e.getMessage());
+			        }
+			        
+			        return null;
+			    }
 	
 	public Boolean modificaCliente(Cliente cliente) {
 		Connection co =null;
