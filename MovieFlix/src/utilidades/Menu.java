afd946599dffda9f52e74bde597b3fdc89b6f49c
Josemi <josemiguelteruelrebollo@gmail.com>
2019-07-30 10:19:34 +0200

    Listar Informes

--- MovieFlix/src/utilidades/Menu.java
+++ MovieFlix/src/utilidades/Menu.java
@@ -12,5 +12,15 @@ public class Menu {
 		System.out.println("7. Informes");
 		System.out.println("0. Salir");
 	}
+	
+	public static void listarInformes() {
+		
+		System.out.println("1 - Listado Peliculas");		
+		System.out.println("2 - Listado Peliculas por Categoria");	
+		System.out.println("3 - Listado Peliculas por Valoracion");	
+		System.out.println("4 - Listado Catalogo Cliente");		
+		System.out.println("5 - Marcar Pelicula cliente como vista");		
+		System.out.println("0 - Salir");
+	}
 
 }
