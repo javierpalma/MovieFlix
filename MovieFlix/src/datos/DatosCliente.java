c8e5149e563350a81b2ace6c00a221123a55be2a
Edmund Home <edmundhome@hotmail.com>
2019-07-31 08:23:01 +0200

    Update DatosCliente.java

--- MovieFlix/src/datos/DatosCliente.java
+++ MovieFlix/src/datos/DatosCliente.java
@@ -196,6 +196,15 @@ public class DatosCliente {
 	 */
 	public void altaCliente(Cliente cliente) {
 		
+		/*
+		 * Connection co = null;
+		 * ConectarBD con= new ConectarBD();
+		 * co=con.conectarBD("movieflix");
+		 * 
+		 * Usamos mejor el try with resources
+		 * 
+		 */
+		
 			try(Connection co=new ConectarBD().conectarBD("movieflix")) {
 				PreparedStatement pt= co.prepareStatement("INSERT INTO cliente (nombre_cliente,fecha_nacimiento,ciudad) VALUES ( '"+cliente.getNombreCliente()+"','"+cliente.getFechaNacimiento()+"','"+cliente.getCiudad()+"');");
 				System.out.println(pt);
